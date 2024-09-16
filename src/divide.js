0c740c0cf076890731ee33754dd5cd670f4c6e32
Priya Sidhaiyan <priyasidhaiyan@priyas-mbp.home>
2024-09-17 00:29:14 +0200

    modified divide.js

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,20 @@
-function divide () {
-       
-}
+function divide(a, b) {
+    // Check if either of the arguments is undefined
+    if (a === undefined || b === undefined) {
+      return undefined;
+    }
+  
+    // Ensure that both arguments are numbers
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      return NaN;
+    }
+  
+    // Return NaN if attempting to divide by zero
+    if (b === 0) {
+      return NaN;
+    }
+  
+    // Return the division result
+    return a / b;
+  }
+  
