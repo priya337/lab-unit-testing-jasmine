bc9df60ef91e3bd0d334b30903e47733a2b98be0
Priya Sidhaiyan <priyasidhaiyan@priyas-mbp.home>
2024-09-17 00:11:39 +0200

    Modified-add.js

--- src/add.js
+++ src/add.js
@@ -1,7 +1,15 @@
-function add (numOne, numTwo) {
-    if (numOne == undefined || numTwo == undefined) {
+//function add (numOne, numTwo) {
+//    if (numOne == undefined || numTwo == undefined) {
+//        return undefined;
+//    }
+//
+//    return numOne + numTwo;
+//}
+
+function add(numOne, numTwo) {
+    // Check if either numOne or numTwo is undefined or not a number
+    if (typeof numOne !== 'number' || typeof numTwo !== 'number') {
         return undefined;
     }
-
     return numOne + numTwo;
 }
